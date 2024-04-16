4912bf2a7e33fa15cda5b4520aa7bf887536ed4f
John Acosta <146490109+johnacosta23@users.noreply.github.com>
2024-04-16 09:47:50 +0200

    Update kernelimg.sh

--- kernelimg.sh
+++ kernelimg.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#! /bin/bash
 
-sudo rm -rf /boot/*.img
+var=$(grubby --info=ALL | grep title | awk '{print $5}' | tr -d '()' | head -1)
+sudo rm -rf /boot/initramfs-$var.img
 sudo reboot
